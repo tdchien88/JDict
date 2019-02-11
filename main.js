350fbb6a3a9a000fd9e831ed14831fedaa09faa9
Tran Duc Chien <tdchien@brights.vn>
2019-02-11 23:46:41 +0900

    disable drag

--- main.js
+++ main.js
@@ -211,30 +211,6 @@ $(window).on('load', function() {
 
 	// target elements with the "draggable" class
 	interact('.draggable')
-	.draggable({
-		// enable inertial throwing
-		inertia: true,
-		// keep the element within the area of it's parent
-		restrict: {
-			restriction: "parent",
-			endOnly: true,
-			elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-		},
-		// enable autoScroll
-		autoScroll: true,
-
-		// call this function on every dragmove event
-		onmove: dragMoveListener,
-		// call this function on every dragend event
-		onend: function (event) {
-			/*var textEl = event.target.querySelector('p');
-
-			textEl && (textEl.textContent = 'moved a distance of '
-			+ (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
-			Math.pow(event.pageY - event.y0, 2) | 0))
-			.toFixed(2) + 'px');*/
-		}
-	})
   	.resizable({
 		// resize from all edges and corners
 		edges: { left: true, right: true, bottom: true, top: true },
@@ -253,7 +229,7 @@ $(window).on('load', function() {
 
 		inertia: true,
 	});
-	
+
 	function resizeMoveListener  (event) {
 		var target = event.target,
 		x = (parseFloat(target.getAttribute('data-x')) || 0),
@@ -276,6 +252,33 @@ $(window).on('load', function() {
  
 	}
 
+/*
+
+	interact('.draggable')
+	.draggable({
+		// enable inertial throwing
+		inertia: true,
+		// keep the element within the area of it's parent
+		restrict: {
+			restriction: "parent",
+			endOnly: true,
+			elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+		},
+		// enable autoScroll
+		autoScroll: true,
+
+		// call this function on every dragmove event
+		onmove: dragMoveListener,
+		// call this function on every dragend event
+		onend: function (event) {
+			//var textEl = event.target.querySelector('p');
+
+//			textEl && (textEl.textContent = 'moved a distance of '
+//			+ (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+//			Math.pow(event.pageY - event.y0, 2) | 0))
+//			.toFixed(2) + 'px');
+		}
+	});
 	function dragMoveListener (event) {
 		var target = event.target,
 		// keep the dragged position in the data-x/data-y attributes
@@ -290,5 +293,6 @@ $(window).on('load', function() {
 		target.setAttribute('data-x', x);
 		target.setAttribute('data-y', y);
 	}
+*/
 
 });
