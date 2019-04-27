a02c50186b62ede31244962ca3ed9433d05e1ac6
Tran Duc Chien <tdchien@brights.vn>
2019-04-27 20:04:40 +0900

    Q

--- WebContent/js/ctrl/n2/n2goi-ctrl.js
+++ WebContent/js/ctrl/n2/n2goi-ctrl.js
@@ -42,7 +42,7 @@ myApp.controller("n2goiCtrl", ["$scope", "$stateParams", 'localStorageService', 
         $scope.data.wrongCount_Kana1 = 4;
         $scope.data.wrongCount_Kana2 = 6;
         $scope.data.wrongCountMin = 2;
-        $scope.data.wrongCountMax = 10;
+        $scope.data.wrongCountMax = 20;
 
         $scope.fcardChecked = false;
 
