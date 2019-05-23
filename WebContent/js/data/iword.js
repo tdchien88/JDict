7315df198a4acf7e5bb8143e35717404fc58fa37
ChienTD <chien.tranduc@isd.co.jp>
2019-05-23 17:30:38 +0900

    xx

---
+++ WebContent/js/data/iword.js
@@ -0,0 +1,7 @@
+myApp.constant('iword',  [
+
+{no:1, tag:`Denwa`, word:`- 電話に出られなかった`, mean:`- I wasn't able to pick up your call.`},
+{no:2, tag:`Denwa`, word:`- 電話に気がつかなくて謝りたい`, mean:`- I'm sorry that I didn't notice your call.`},
+{no:3, tag:`Denwa`, word:`- おかけになった電話番号は現在使われておりません`, mean:`- The phone number you called is not currently used.`},
+
+]);
