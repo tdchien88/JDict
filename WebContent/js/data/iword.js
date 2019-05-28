35f166b8aaa29f9b40a326bd52b63e48e2e8623d
Tran Duc Chien <tdchien@brights.vn>
2019-05-28 21:56:12 +0900

    な

--- WebContent/js/data/iword.js
+++ WebContent/js/data/iword.js
@@ -4,4 +4,8 @@ myApp.constant('iword',  [
 {no:2, tag:`Denwa`, word:`- 電話に気がつかなくて謝りたい`, mean:`- I'm sorry that I didn't notice your call.`},
 {no:3, tag:`Denwa`, word:`- おかけになった電話番号は現在使われておりません`, mean:`- The phone number you called is not currently used.`},
 
+{no:4, tag:`天気`, word:`雨が降らないそう。 `, mean:`I heard it won't rain.`},
+{no:5, tag:`天気`, word:`雨が降りそうもない。`, mean:`I think it won't rain.`},
+{no:6, tag:`天気`, word:`雨が降らなさそう。`, mean:`I think it won't rain.`},
+
 ]);
