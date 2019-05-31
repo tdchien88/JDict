bf65edda5cc6bd2a3b3c83c1c62c911d17ebef10
Tran Duc Chien <tdchien@brights.vn>
2019-06-01 00:12:30 +0900

    Update utils.js

--- WebContent/js/common/utils.js
+++ WebContent/js/common/utils.js
@@ -1,3 +1,39 @@
+
+
+// REFERENCE UNICODE TABLES: 
+// http://www.rikai.com/library/kanjitables/kanji_codes.unicode.shtml
+// http://www.tamasoft.co.jp/en/general-info/unicode.html
+//
+// TEST EDITOR:
+// http://www.gethifi.com/tools/regex
+//
+// UNICODE RANGE : DESCRIPTION
+// 
+// 3000-303F : punctuation
+// 3040-309F : hiragana
+// 30A0-30FF : katakana
+// FF00-FFEF : Full-width roman + half-width katakana
+// 4E00-9FAF : Common and uncommon kanji
+// 
+// Non-Japanese punctuation/formatting characters commonly used in Japanese text
+// 2605-2606 : Stars
+// 2190-2195 : Arrows
+// u203B     : Weird asterisk thing
+
+var regexJPString = /[\u3000-\u303F]|[\u3040-\u309F]|[\u30A0-\u30FF]|[\uFF00-\uFFEF]|[\u4E00-\u9FAF]|[\u2605-\u2606]|[\u2190-\u2195]|\u203B/g; 
+function isJPString(input){
+//var input = "input string"; 
+var res = false;
+if(regexJPString.test(input)) {
+    console.log("Japanese characters found")
+res = true;
+}
+else {
+    console.log("No Japanese characters");
+}
+return res;
+}
+
 function isEqual(a, b){
     var regex = /[〜’”【】「」。、　 ０-９！＂＃＄％＆＇（）＊＋，－．／：；＜＝＞？＠［＼］＾＿｀｛｜｝～0-9`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi;
 
