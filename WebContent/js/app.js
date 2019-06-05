d555328f38d241a494bc47c9763621712c8906bf
ChienTD <chien.tranduc@isd.co.jp>
2019-06-05 17:32:30 +0900

    x

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -1,8 +1,8 @@
 var myApp = angular.module("myApp", ['ui.router', 'LocalStorageModule', 'ngMaterial', 'ngMessages',
-    "ui.bootstrap", "ui.bootstrap.datepicker", "ui.bootstrap.datepickerPopup", "ui.bootstrap.dateparser", "ui.bootstrap.isClass", "ui.bootstrap.tabs", 'awesome-angular-swipe']);
+    "ui.bootstrap", "ui.bootstrap.datepicker", "ui.bootstrap.datepickerPopup", "ui.bootstrap.dateparser", "ui.bootstrap.isClass", "ui.bootstrap.tabs"]);
 
 
-myApp.run( function($rootScope, $timeout){
+myApp.run( function($rootScope, $timeout, $interval){
 	$rootScope._loading = false;
 	$rootScope.showLoading = function(callback){
         $rootScope._loading = true;
@@ -11,6 +11,20 @@ myApp.run( function($rootScope, $timeout){
             $rootScope._loading = false;
         }, 0);
 	}
+
+	$rootScope.clientCurrent;
+	$rootScope.clientCurrentPoint;
+
+    var dateProgress = $interval(function() {
+        setDate();
+    }, 1000);
+
+    setDate();
+
+    function setDate() {
+        $rootScope.clientCurrent = new Date();
+        $rootScope.clientCurrentPoint = ($rootScope.clientCurrent.getSeconds() % 2 == 0);
+    }
 
 });
 
